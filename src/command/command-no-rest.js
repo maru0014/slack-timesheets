9ebc0c27d8886ac8fbae6e396579b49c867c4e08
nodirashidov <nrashidov@yahoo.com>
2017-05-29 14:36:04 +0900

    fixed: SignOut cannot be blank to register rest hours, renamed the command file

--- src/command/command-no-rest.js
+++ src/command/command-no-rest.js
@@ -4,7 +4,7 @@ import TimesheetRow from '../timesheet-row';
 
 import moment from 'moment';
 
-export default class CommandNoBreak extends CommandAbstract{
+export default class CommandNoRest extends CommandAbstract{
   static match(body) {
     return body.match(/休憩なし/);
   }
@@ -26,7 +26,7 @@ export default class CommandNoBreak extends CommandAbstract{
     const now = moment();
     const row = this.timesheets.get(username, date? date: now);
 
-    if (row.getSignIn() && row.getSignIn() !== '-') {
+    if (row.getSignIn() && row.getSignIn() !== '-' && row.getSignOut() && row.getSignOut() !== '-') {
       let restTime = 0;
       let workedHours = TimesheetRow.workedHours(row.getSignIn(), row.getSignOut(), restTime);
 
@@ -41,6 +41,11 @@ export default class CommandNoBreak extends CommandAbstract{
 
       this.commandTotal.execute(username, date, time);
 
+    }
+    else if ((row.getSignIn() && row.getSignIn() !== '-') && (!row.getSignOut() || row.getSignOut() === '-')) {
+      this.slack.send(
+        "@"+username+" "+(date? date.format('YYYY/MM/DD'): now.format('YYYY/MM/DD'))+"このコマンドを退勤してから実行してください"
+      );
     }
     else {
       this.slack.send(
--- src/command/dispatcher.js
+++ src/command/dispatcher.js
@@ -3,7 +3,7 @@ import CommandHelp from './command-help';
 import CommandSignIn from './command-sign-in';
 import CommandSignOut from './command-sign-out';
 import CommandTotal from "./command-total";
-import CommandNoBreak from "./command-no-break";
+import CommandNoRest from "./command-no-rest";
 
 
 export default class Dispatcher {
@@ -32,7 +32,7 @@ export default class Dispatcher {
       CommandSignOut,
       CommandSignIn,
       CommandTotal,
-      CommandNoBreak,
+      CommandNoRest,
       CommandHelp
     ];
 
